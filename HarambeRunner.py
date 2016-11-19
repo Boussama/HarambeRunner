5e14033cc3c902520920dad3eb937d1ad579b2fc
Bouthouri Oussama <oussama.bouthouri@gmail.com>
2016-11-19 18:14:23 +0100

    new UI

--- HarambeRunner.py
+++ HarambeRunner.py
@@ -28,7 +28,7 @@ def printcoords(event):
     #outputting x and y coords to console
 	print (mouseX,mouseY)
 	os.system("monkeyrunner HarambeBridge.py " + str(mouseX) + " " + str(mouseY) )
-	print "salam"
+	print ("salam")
 	loadImage()
 
 root = Tk()
@@ -54,10 +54,33 @@ rawImage = rawImage.resize((int(canvasImgWidth), int(canvasImgHeight)) ,Image.AN
 img = ImageTk.PhotoImage(rawImage)
 
 canvas = Canvas(root, bd=0, width=canvasImgWidth, height=canvasImgHeight)
+canvas.grid(row=0, column=0, rowspan=8)
 canvasImageRef = canvas.create_image(0,0,image=img, anchor="nw")
+
+coordLabel = Label(root, text="Coord (0,0)")
+coordLabel.grid(row=0, column=1, sticky=W)
+
+delayLabel = Label(root, text="Delay")
+delayLabel.grid(row=1, column=1, sticky=W)
+
+delayEntry = Entry(root, bg="blue")
+delayEntry.grid(row=1, column=2)
+delayEntry.insert(0,"1")
+
+nextButton = Button(root, text="Next")
+nextButton.grid(row=2, column=1)
+
+executeButton = Button(root, text="Execute")
+executeButton.grid(row=2, column=2)
+
+logLabel = Label(root, text="Log :")
+logLabel.grid(row=3, column=1, sticky=W)
+
+logText = Text(root, bg="red")
+logText.grid(row=4, column=1, columnspan=4 )
+logText.insert(END, "il vaut mieux avoir affaire a dieu qua ses saints")
 
 #mouseclick event
 canvas.bind("<Button 1>",printcoords)
 
-canvas.pack()
 root.mainloop()
